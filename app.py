f4fbc517c38568178f6c42fb8134ea48f5aec9d9
emnaidoudi <emnaidoudi@outlook.com>
2019-02-22 10:42:50 +0100

    careful

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from mathi import *
 from framework import response
 from chatterbot import ChatBot
+from flask_cors import CORS
+from flask_pymongo import PyMongo,MongoClient
+import os
+from bson.json_util import dumps
+import json
+from model import *
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+client = MongoClient(
+    "mongodb://localhost:27017/")
+db = client.mydb
+collection=db["intents"]
 
 bot_answer=""
 bot = ChatBot(
@@ -15,20 +26,61 @@ bot = ChatBot(
    ])  
 
 
+#--------------------------------------INTENTS CRUD ---------------------------------------------------------
 
-#--------------------------------------INTENTS---------------------------------------------------------
-@app.route("/api/chatbot/intents",methods=['POST'])
-def fill_intents_file(intents):
+@app.route("/api/chatbot/crud/intents",methods=['POST'])
+def post_intents(intents):
+    try:
+        collection.insert_one(intents)
+        intents=db.intents.find( { },{"tag":1,"patterns":1,"responses":1,"_id":0})
+        return Response(dumps(intents),status=201) # The request has succeeded and a new resource has been created as a result of it.
+    except:
+        return Response(status=405)
+
+
+@app.route("/api/chatbot/crud/intents",methods=['GET'])
+def get_intents():
+    #save_to_intents(dumps(intents))
+    intents=db.intents.find( { },{"tag":1,"patterns":1,"responses":1,"_id":0})
+    return Response(dumps(intents)  ,mimetype='application/json',status=200)
+    
+
+
+@app.route("/api/chatbot/crud/intents/<string:tag>",methods=["DELETE"]) 
+def delete_intent(tag):
+    try:
+        collection.delete_one({ "tag" : tag })
+        return Response(status=200)
+    except:
+        return Response(status=405) #Method not allowed  
+
+
+@app.route("/api/chatbot/crud/intents")
+def update_intent():
     pass
-#--------------------------------------------------------------------------------------------------------
-@app.route("/api/chatbot/intents",methods=['GET'])
-def get_intents_file(intents):
-    pass
-#-----------------------------------------------MATH------------------------------------------------------
+
+
+
+
+
+
+
+#---------------------------------------TRAIN MODEL-------------------------------------------------
+@app.route("/api/chatbot/train")
+def train():
+    train_model()
+    
+    return Response(status=200)
+    #os.system("./model.py")
+    #execfile("./filename")
+
+#-----------------------------------------------------------------------------------------------------
+
 @app.route("/api/chatbot/math/<string:sentence>", methods=['GET'])
 def mathApi(sentence):
     return jsonify({"result":mathStuff(sentence)}) #"5 + 5"
 #-----------------------------------------------------------------------------------------------------    
+
 @app.route("/api/chatbot/basic/<string:sentence>")
 def basic(sentence):
     try:
@@ -37,7 +89,6 @@ def basic(sentence):
     except: 
         return  jsonify({"response":response(sentence)})  
 
-        
     
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.stem.snowball import FrenchStemmer
-
+import json
+import pickle
 stemmer = LancasterStemmer()
 fstemmer = FrenchStemmer()
 
@@ -10,103 +11,102 @@ import tflearn
 import tensorflow as tf
 import random
 
-# import our chat-bot intents file
-import json
-with open('intents.json') as json_data:
-    intents = json.load(json_data)
-
-
 def engilsh_french_stem(sentence):
     french=fstemmer.stem(sentence)
     english=stemmer.stem(sentence)
     return french if len(french)< len(english) else english
 
+def preprocessing1():
+    with open('intents.json') as json_data:
+        intents = json.load(json_data)
+    words = [] #Design the Vocabulary (unique words)
+    classes = []
+    documents = []
+    ignore_words = ['?']
+    # loop through each sentence in our intents patterns
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            # tokenize each word in the sentence
+            w = nltk.word_tokenize(pattern)
+            # add to our words list
+            words.extend(w)
+            # add to documents in our corpus
+            documents.append((w, intent['tag']))
+            # add to our classes list
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])    
+    # stem and lower each word and remove duplicates
+    words = [engilsh_french_stem(w.lower()) for w in words if w not in ignore_words]
+    words = sorted(list(set(words)))
+    # remove duplicates
+    classes = sorted(list(set(classes)))
+    return (classes,words,documents)    
 
-words = [] #Design the Vocabulary (unique words)
-classes = []
-documents = []
-ignore_words = ['?']
-# loop through each sentence in our intents patterns
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = nltk.word_tokenize(pattern)
-        # add to our words list
-        words.extend(w)
-        # add to documents in our corpus
-        documents.append((w, intent['tag']))
-        # add to our classes list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])    
+#---------------------------------------------------------------------------------------------------------------------------------------------------
 
+def preprocessing2():
+    all=preprocessing1()
+    classes=all[0]
+    words=all[1]
+    documents=all[2]
+    
+    # create our training data
+    training = []
+    output = []
 
-# stem and lower each word and remove duplicates
-words = [engilsh_french_stem(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
+    # create an empty array for our output
+    output_empty = [0] * len(classes)
 
-# remove duplicates
-classes = sorted(list(set(classes)))
+    # training set, bag of words for each sentence
+    for doc in documents:
+        # initialize our bag of words
+        bag = []
+        # list of tokenized words for the pattern (pattern = what user says)
+        pattern_words = doc[0] 
+        # stem each word
+        pattern_words = [engilsh_french_stem(word.lower()) for word in pattern_words]
+        # create our bag of words array
+        # mark the presence of words as a boolean value, 0 for absent, 1 for present.
+        for w in words:
+            bag.append(1) if w in pattern_words else bag.append(0)
 
-print (len(documents), "documents")
-print (len(classes), "classes", classes)
-print (len(words), "unique stemmed words", words)
+        # output is a '0' for each tag and '1' for current tag
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
 
+        training.append([bag, output_row])
 
+        # shuffle our features and turn into np.array
+        random.shuffle(training)
+        training = np.array(training)
 
-# create our training data
-training = []
-output = []
+        # create train and test lists
+        train_x = list(training[:,0])
+        train_y = list(training[:,1])
 
-# create an empty array for our output
-output_empty = [0] * len(classes)
+        return (train_x,train_y,classes,words)
 
-# training set, bag of words for each sentence
-for doc in documents:
-    # initialize our bag of words
-    bag = []
-    # list of tokenized words for the pattern (pattern = what user says)
-    pattern_words = doc[0] 
-    # stem each word
-    pattern_words = [engilsh_french_stem(word.lower()) for word in pattern_words]
-    # create our bag of words array
-    # mark the presence of words as a boolean value, 0 for absent, 1 for present.
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
+def train_model():
+    train=preprocessing2()
+    train_x=train[0]
+    train_y=train[1]
+    classes=train[2]
+    words=train[3]  
+    # reset underlying graph data
+    tf.reset_default_graph()
+    # Build neural network
+    net = tflearn.input_data(shape=[None, len(train_x[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+    net = tflearn.regression(net)
 
-    # output is a '0' for each tag and '1' for current tag
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+    # Define model and setup tensorboard
+    model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+    # Start training (apply gradient descent algorithm)
+    model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
 
-    training.append([bag, output_row])
-
-
-# shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-
-# create train and test lists
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-
-
-# reset underlying graph data
-tf.reset_default_graph()
-# Build neural network
-net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net)
-
-# Define model and setup tensorboard
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# Start training (apply gradient descent algorithm)
-model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-
-#--------------------------------------SAVE---------------------------------------------
-model.save('model.tflearn')
-
-
-# save all of our data structures
-import pickle
-pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
+    #--------------------------------------SAVE---------------------------------------------
+    model.save('model.tflearn')
+    # save all of our data structures
+    pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
